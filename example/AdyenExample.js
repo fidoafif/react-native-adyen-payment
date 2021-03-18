b0553cb096553b925e2149722355e963b6130b0d
Fido Fadillah <fido.fadillah@flash-coffee.com>
2021-03-18 15:04:37 +0800

    Update example

--- example/AdyenExample.js
+++ example/AdyenExample.js
@@ -1,24 +1,32 @@
 import React, {useState, useEffect} from 'react';
-import {Platform, View, Button, Text} from 'react-native';
-import AdyenPayment from 'react-native-adyen-payment';
 import {
-  ADYEN_MERCHANT_ACCOUNT,
-  ADYEN_PUBLIC_KEY,
-  ADYEN_BASE_URL,
-  ADYEN_ENVIRONMENT,
-} from '@env';
+  Platform,
+  View,
+  Alert,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
+import AdyenPayment from 'react-native-adyen-payment';
+import Config from 'react-native-config';
+
+const ADYEN_MERCHANT_ACCOUNT = Config.ADYEN_MERCHANT_ACCOUNT;
+const ADYEN_PUBLIC_KEY = Config.ADYEN_PUBLIC_KEY;
+const ADYEN_BASE_URL = Config.ADYEN_BASE_URL;
+const ADYEN_ENVIRONMENT = Config.ADYEN_ENVIRONMENT;
+const X_DEMO_SERVER_KEY = Config.X_DEMO_SERVER_KEY;
 
 const MOCK_PAYMENT_DETAILS = {
   amount: {
     value: 100,
-    currency: 'USD',
+    currency: 'SGD',
   },
-  reference: '123456',
-  shopperReference: '123456',
+  reference: 'f685d187-7f4d-4dd5-9368-b84a3039f934',
+  shopperReference: 'f685d187-7f4d-4dd5-9368-b84a3039f934',
   shopperEmail: 'test@test.test',
   shopperLocale: 'en_US',
   channel: Platform.OS === 'ios' ? 'iOS' : 'Android',
-  countryCode: 'US',
+  countryCode: 'SG',
   // Remember to replace returnUrl with your app scheme
   returnUrl:
     Platform.OS === 'ios' ? 'your-ios-scheme://' : 'your-android-scheme://',
@@ -35,8 +43,8 @@ const MOCK_COMPONENT_DATA = {
   },
   // Uncomment to add Apple Pay (replace apple_pay_merchant_id):
   // applepay: {
-  //   apple_pay_merchant_id: 'merchant.com.adyen.your.merchant',
-  //   supportedNetworks: ['visa', 'masterCard', 'amex', 'discover'],
+  //   apple_pay_merchant_id: ADYEN_MERCHANT_ACCOUNT, //'merchant.com.adyen.your.merchant',
+  //   supportedNetworks: ['visa', 'mc'],
   //   merchantCapabilities: ['supports3DS'],
   // },
 };
@@ -47,6 +55,7 @@ const APP_SERVICE_CONFIG_DATA = {
   // Add any additional headers to pass to your backend
   additional_http_headers: {
     'x-channel': Platform.OS, // Example
+    'x-demo-server-api-key': X_DEMO_SERVER_KEY,
   },
 };
 
@@ -57,42 +66,149 @@ const STATUS = {
   failure: 'failure',
 };
 
-function AdyenExample() {
+const MOCK_PAYMENT_METHODS = {
+  paymentMethods: [
+    {
+      brands: ['visa', 'mc', 'amex', 'jcb'],
+      details: [
+        {key: 'number', type: 'text'},
+        {key: 'expiryMonth', type: 'text'},
+        {key: 'expiryYear', type: 'text'},
+        {key: 'cvc', type: 'text'},
+        {key: 'holderName', optional: true, type: 'text'},
+      ],
+      name: 'Credit Card',
+      type: 'scheme',
+    },
+    {name: 'GrabPay', type: 'grabpay_SG'},
+    {
+      configuration: {
+        merchantId: '1000',
+        gatewayMerchantId: 'DigitalServicesSGFourPteLtdECOM',
+      },
+      details: [{key: 'paywithgoogle.token', type: 'payWithGoogleToken'}],
+      name: 'Google Pay',
+      type: 'paywithgoogle',
+    },
+    {name: 'UnionPay', type: 'unionpay'},
+  ],
+};
+
+export const AdyenExample = () => {
   const [status, setStatus] = useState(STATUS.none);
 
   useEffect(() => {
+    console.log(APP_SERVICE_CONFIG_DATA);
     AdyenPayment.initialize(APP_SERVICE_CONFIG_DATA);
 
     AdyenPayment.onSuccess((payload) => {
       console.log('success', payload);
+      Alert.alert('Success', JSON.stringify(payload));
       setStatus(STATUS.success);
     });
     AdyenPayment.onError((payload) => {
       console.log('failure', payload);
+      Alert.alert('Failure', JSON.stringify(payload));
       setStatus(STATUS.failure);
     });
   }, []);
 
-  function handleDropInButtonPress() {
+  const handleDropInButtonPress = () => {
+    console.log('handleDropInButtonPress');
+    setStatus(STATUS.initiated);
+
+    try {
+      AdyenPayment.startCustomPayment(
+        AdyenPayment.DROPIN,
+        MOCK_COMPONENT_DATA,
+        MOCK_PAYMENT_DETAILS,
+        MOCK_PAYMENT_METHODS
+      );
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleCCButtonPress = () => {
+    console.log('handleDropInButtonPress');
     setStatus(STATUS.initiated);
 
     try {
       AdyenPayment.startPayment(
-        AdyenPayment.DROPIN,
+        AdyenPayment.SCHEME,
         MOCK_COMPONENT_DATA,
         MOCK_PAYMENT_DETAILS,
       );
     } catch (err) {
       console.error(err);
     }
-  }
+  };
+
+  const handleAFTERPAYButtonPress = () => {
+    console.log('handleAFTERPAYButtonPress');
+    setStatus(STATUS.initiated);
+
+    try {
+      AdyenPayment.startPayment(
+        AdyenPayment.AFTERPAY,
+        MOCK_COMPONENT_DATA,
+        MOCK_PAYMENT_DETAILS,
+      );
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleAPPLEPAYButtonPress = () => {
+    console.log('handleAPPLEPAYButtonPress');
+    setStatus(STATUS.initiated);
+
+    try {
+      AdyenPayment.startPayment(
+        AdyenPayment.APPLE_PAY,
+        MOCK_COMPONENT_DATA,
+        MOCK_PAYMENT_DETAILS,
+      );
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
-    <View>
-      <Text>Status: {status}</Text>
-      <Button title="Drop-in" onPress={handleDropInButtonPress} />
+    <View style={{alignItems: 'center'}}>
+      {/* <Text>Status: {status}</Text> */}
+      <View style={{marginBottom: 34}} />
+
+      <TouchableOpacity style={styles.button} onPress={handleDropInButtonPress}>
+        <Text>Drop-In</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button} onPress={handleCCButtonPress}>
+        <Text>Credit Card</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={handleAFTERPAYButtonPress}>
+        <Text>AfterPay</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={handleAPPLEPAYButtonPress}>
+        <Text>ApplePay</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
-export default React.memo(AdyenExample);
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    paddingHorizontal: 34,
+    paddingVertical: 16,
+    backgroundColor: '#FA275A',
+    width: 180,
+    marginVertical: 8,
+  },
+});
